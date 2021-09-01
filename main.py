7349b6eadf83c92184678b4ead13d23018b7c17f
Aizenz <jt-lufei@hotmail.com>
2021-09-01 18:40:44 +0800

    robustness of stratum

--- main.py
+++ main.py
@@ -1,41 +1,12 @@
+import datetime
 import numpy as np
 import relationship
 import pandas as pd
 import networkx as nx
-import matplotlib.pyplot as plt
 from scipy import stats
 
-if __name__ == "test":
-    df = pd.read_excel('/Users/aizenz/Desktop/internHI/ideas_u20210811202801.xlsx')
-    industries = df['industry'].unique()
-    industries = np.sort(industries)
-    i = 1
-    plt.figure(figsize=(17, 30))
-
-    for industry in industries:
-        field = df[df['industry'] == industry]
-        matrix = relationship.relationmatrix(field, by='industry')
-        matrix = matrix.to_numpy()
-        matrix = devide(matrix)
-        G = nx.DiGraph(matrix)
-        i += 1
-        plt.subplot(10, 3, i)
-        plt.title(industry)
-
-        strenth = 1
-        elarge = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] < strenth]
-        esmall = [(u, v) for (u, v, d) in G.edges(data=True) if d['weight'] >= strenth]
-        pos = nx.spring_layout(G)
-        nx.draw_networkx_nodes(G, pos, node_size=0.7)
-        nx.draw_networkx_edges(G, pos, edgelist=elarge, width=2, edge_color='r', arrows=False)
-        nx.draw_networkx_edges(G, pos, edgelist=esmall, width=0.2, alpha=0.5, edge_color='b', arrows=False)
-        # labels标签定义
-        # nx.draw_networkx_labels(G, pos, labels=None)
-    plt.tight_layout(h_pad=2, w_pad=2)
-    plt.savefig("weighted_graph.png")
-    plt.show()
-
-if __name__ == "__main__":
+# By idea level
+if __name__ == "1":
     df = pd.read_excel('/Users/aizenz/Desktop/internHI/ideasData/lifetimeIdeas_u20210901103447.xlsx')
     matrix = relationship.relationmatrix(df, by='ticker', pair_name='idea_entity_id')
     df.lifetimeAlpha = df['lifetimeAlpha']/df['size']
@@ -54,3 +25,60 @@ if __name__ == "__main__":
     res.columns = ['follower_idea', 'leader_idea', 'PR']
     res = res[res['PR'] != 0].reset_index().drop(columns='index')
     res.to_csv('ideaPR.csv')
+
+# By contributor level
+
+if __name__ =='2':
+    df = pd.read_excel('/Users/aizenz/Desktop/internHI/ideasData/lifetimeIdeas_u20210901103447.xlsx')
+    matrix = relationship.relationmatrix(df, by='ticker', pair_name='creator')
+    df.lifetimeAlpha = df['lifetimeAlpha'] / df['size']
+    # lifetimeAlpha by creator
+    creatorAlpha = df[['creator', 'lifetimeAlpha']].groupby(['creator']).mean()
+    # Pagerank by leader
+    G = nx.DiGraph(matrix.to_numpy())
+    pr = nx.pagerank(G, alpha=0.9)
+    pr = pd.DataFrame(pr, index=[0]).T.set_axis(matrix.index).reset_index()
+    pr.columns = ['creator', 'PR']
+    pr = pr.sort_values(by='PR', ascending=False)
+    pr = pd.merge(pr, creatorAlpha, on='creator')
+    pr.to_csv('PRleader.csv')
+    # Spearman correlation
+    print('leader:', stats.spearmanr(pr['PR'], pr['lifetimeAlpha'], alternative='greater'))
+
+    # Pagerank by follower, using the reverse matrix
+    G = nx.DiGraph(matrix.to_numpy().T)
+    pr = nx.pagerank(G, alpha=0.9)
+    pr = pd.DataFrame(pr, index=[0]).T.set_axis(matrix.index).reset_index()
+    pr.columns = ['creator', 'PR']
+    pr = pr.sort_values(by='PR', ascending=False)
+    pr = pd.merge(pr, creatorAlpha, on='creator')
+    pr.to_csv('PRfollower.csv')
+    # Spearman correlation
+    print('follower:', stats.spearmanr(pr['PR'], pr['lifetimeAlpha'], alternative='greater'))
+
+    # save the pivot
+    res = matrix.stack().reset_index()
+    res.columns = ['follower_idea', 'leader_idea', 'PR']
+    res = res[res['PR'] != 0].reset_index().drop(columns='index')
+    res.to_csv('ideaPR.csv')
+
+# The resignation
+if __name__ =='__main__':
+    df = pd.read_excel('/Users/aizenz/Desktop/internHI/ideasData/lifetimeIdeas_u20210901103447.xlsx')
+    dfBefore = df[df['create_time'] < np.datetime64('2021-08-01')]
+    dfAfter = df[df['create_time'] >= np.datetime64('2021-08-01')]
+    matrixBefore = relationship.relationmatrix(dfBefore, by='ticker', pair_name='creator')
+    matrixAfter = relationship.relationmatrix(dfAfter, by='ticker', pair_name='creator')
+
+    prBefore = pd.DataFrame(nx.pagerank(nx.DiGraph(matrixBefore.to_numpy()), alpha=0.9), index=[0]).T.set_axis(matrixBefore.index).reset_index()
+    prBefore.columns = ['creator', 'PRBeforeAug']
+    prBefore = prBefore.sort_values(by='PRBeforeAug', ascending=False)
+
+    prAfter = pd.DataFrame(nx.pagerank(nx.DiGraph(matrixAfter.to_numpy()), alpha=0.9), index=[0]).T.set_axis(matrixAfter.index).reset_index()
+    prAfter.columns = ['creator', 'PRAfterAug']
+    prAfter = prAfter.sort_values(by='PRAfterAug', ascending=False)
+    # Spearman correlation
+    pr = pd.merge(prBefore, prAfter, on=['creator'], how='inner')
+    pr.to_excel('robustness.xlsx')
+    print(stats.spearmanr(pr['PRBeforeAug'], pr['PRAfterAug']))
+
